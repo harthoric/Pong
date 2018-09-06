5e6edef3e7372f607330d2048686d8249c24bb58
harthoric <harthoric@gmail.com>
2018-09-06 18:12:42 +0100

    Created project

---
+++ src/Main.cpp
@@ -0,0 +1,13 @@
+//============================================================================
+// Name        : Game.cpp
+// Author      : Harthoric
+// Version     :
+// Copyright   : Free to use
+// Description : Pong in C++, Ansi-style
+//============================================================================
+
+#include "Game.h"
+
+int main(int argc, char** argv) {
+	return 0;
+}
