13c064579983e37ae91cb53252003836f0b00421
harthoric <harthoric@gmail.com>
2018-09-06 18:19:51 +0100

    Created game class and game variables

---
+++ src/Game.cpp
@@ -0,0 +1,6 @@
+/*
+ * Game.cpp
+ *
+ *  Created on: 4 Sep 2018
+ *      Author: harthoric
+ */
---
+++ src/Game.h
@@ -0,0 +1,49 @@
+/*
+ * Game.h
+ *
+ *  Created on: 4 Sep 2018
+ *      Author: harthoric
+ */
+
+#pragma once
+#include "SDL.h"
+
+class Game {
+public:
+  Game();
+	// initialises game
+	bool Initialize();
+	// runs game loop until the game has finished
+	void RunLoop();
+	// shutdowns the game
+	void Shutdown();
+private:
+	void ProcessInput();
+	void UpdateGame();
+	void GenerateOutput();
+	void resetBall();
+
+	// SDL window
+	SDL_Window* mWindow;
+	// 2D renderer
+	SDL_Renderer* mRenderer;
+	// holds number of ticks since game's start
+	Uint32 mTicksCount;
+	// is the game running
+	bool mIsGameRunning;
+
+	// direction of paddle 1
+	int mPaddle1Direction;
+	// position of paddle 1
+	Vector2 mPaddlePosition1;
+
+	// direction of paddle 2
+	int mPaddleDirection2;
+	// position of paddle 2
+	Vector2 mPaddlePos2;
+  
+	// position of ball
+	Vector2 mBallPosition;
+	// velocity of ball
+	Vector2 mBallVelocity;
+};
